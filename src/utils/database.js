30710a8efc52515fde84967211496d7dc508a312
Gilbert3052 <gilbertfernandez3052@gmail.com>
2022-11-29 17:37:54 -0400

    Creación projecto

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,2 +1,13 @@
+const {Sequelize} = require('sequelize')
+
+const db = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'postgres',
+    password: 'root',
+    database: 'users'
+})
 
 
+module.exports = db
